43651f057ba45cd6aed48edac9baf9d9b839403e
Wichait <64070230@kmitl.ac.th>
2021-10-21 12:44:20 +0700

    21/10/2021/12:44

--- inside.py
+++ inside.py
@@ -4,7 +4,7 @@ from flask.templating import render_template
 
 app = Flask(__name__)
 
-@app.route("/home")
+@app.route("/")
 def home():
     return render_template('home.html')
 
@@ -19,8 +19,6 @@ def register():
 @app.route("/user_account_login")
 def login():
     return render_template("login.html")
-
-
 
 
 
