5ec5c5071e4901e0da13365b72e938777658c732
Wichait <64070230@kmitl.ac.th>
2021-10-21 23:04:25 +0700

    21/10/2021/32:04/big

--- inside.py
+++ inside.py
@@ -4,19 +4,31 @@ from flask.templating import render_template
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/") #                   home page *
 def home():
     return render_template('home.html')
 
-@app.route("/bmi")
+@app.route("/bmi")#                   BMI page *
 def bmi():
     return render_template("bmi.html")
 
-@app.route("/user_account_register")
+@app.route("/food")#                   food page *
+def food():
+    return render_template("food.html")
+
+@app.route("/about")#                   about page *
+def about():
+    return render_template("about.html")
+
+@app.route("/help")#                  help page *
+def help():
+    return render_template("help.html")
+
+@app.route("/user_account_register")#             register page *
 def register():
     return render_template("register.html")
 
-@app.route("/user_account_login")
+@app.route("/user_account_login")#               login page *
 def login():
     return render_template("login.html")
 
